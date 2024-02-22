4bda52dba0ff33b5d938222570dde3ac34c90b62
Lucas Guerra <lucasmazoniguerra@gmail.com>
2024-02-22 18:03:22 -0300

    color testing

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -27,8 +27,7 @@ void NovaPixelEQAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
+    g.setColour (juce::Colours::deeppink);
     g.setFont (15.0f);
     g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
@@ -38,5 +37,3 @@ void NovaPixelEQAudioProcessorEditor::resized()
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
 }
-
-int kd{ 9 };
